2240c5cf08285041248ee816a6bb4e043a5b6b72
Khenissi Mohamed Habib <khenissimohamed26@gmail.com>
2026-06-30 11:57:48 +0100

    Temperature & Humidity Sensor ( DHT11)
    the measurement of  Temperature & Humidity (°C  /  %)

---
+++ THSensor.ino
@@ -0,0 +1,29 @@
+#include <DHT.h>
+#define DHTPIN 10
+#define DHTTYPE DHT11
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(115200);
+  dht.begin();
+}
+
+void loop() {
+  
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+
+   if (isnan(h) || isnan(t)) {
+    Serial.println(F("Failed to read from DHT sensor!"));
+    return;
+  }
+
+  Serial.print(F("Humidity: "));
+  Serial.print(h);
+  Serial.print(F("%  Temperature: "));
+  Serial.print(t);
+  Serial.println(F("°C"));
+  delay(5000);
+}
+
